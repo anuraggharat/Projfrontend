fe2d25b01efbbba321ccb510f0cd68e63f6f427d
Sahil-Bhave <sahil2299@gmail.com>
2021-10-07 09:58:09 +0530

    grid dependency added

--- src/App/Components/Main.js
+++ src/App/Components/Main.js
@@ -6,19 +6,67 @@ import BarChart from './Graphs/BarChart';
 import CombinedChart from './Graphs/CombinedChart';
 import News from './News';
 import Faqs from './Faqs';
+import {
+  GridContextProvider,
+  GridDropZone,
+  GridItem,
+  swap,
+  move
+} from "react-grid-dnd";
 
 const Main = (props) => {
-
-
-
+  //Detect change in profile selected
     useEffect(() => {
-      console.log(props.some);
+      setItems(props.some);
     }, [props.some])
-    
+
+    const [items, setItems] = React.useState([0,1,2,3]);
+
+    const [val,setVal] = React.useState(true);
+    //Detect change in window size
+    useEffect(() => {
+      function handleResize() {
+        if(window.innerWidth<720){
+          setVal(false);
+        }
+        else{
+          setVal(true);
+        }
+      
+  }
+      window.addEventListener('resize', handleResize)
+    })
+    const chartArr = [<LineChart/>,<BarChart/>,<CombinedChart/>,<LineChart/>];
+    function onChange(sourceId, sourceIndex, targetIndex, targetId) {
+    const nextState = swap(items, sourceIndex, targetIndex);
+    setItems(nextState);
+    console.log(items);
+      
+    }
 
     return (
       <>
-        <div class="container mt-5 mb-5">
+      <GridContextProvider onChange={onChange}>
+      <GridDropZone
+        id="items"
+        boxesPerRow={val==true? 2:1}
+        rowHeight={300}
+        style={{ height: "400px" }}
+      >
+        {items.map(item => (
+          <GridItem key={item} >
+            
+            <div className="card border-0 bg-white rounded shadow-sm m-2" >
+                <div className="card-body">
+              {chartArr[item]}
+              </div>
+              </div>
+            
+          </GridItem>
+        ))}
+      </GridDropZone>
+    </GridContextProvider>
+        {/* <div class="container mt-5 mb-5">
           <div class="row">
             <div className="col-lg-6 mb-5">
               <div className="card border-0 bg-white rounded shadow-sm">
@@ -56,7 +104,7 @@ const Main = (props) => {
               
             </div>
           </div>
-        </div>
+        </div> */}
       </>
     );
 }
