ec06dfd2eceed626663d57bd7257d89befd9bce6
Sahil-Bhave <57219013+Sahil-Bhave@users.noreply.github.com>
2021-10-07 15:16:46 +0530

    Update BarChart.js

--- src/App/Components/Graphs/BarChart.js
+++ src/App/Components/Graphs/BarChart.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 
-const data = {
-  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-  datasets: [
-    {
-      label: "Sales",
-      data: [33, 53, 85, 41, 44, 65],
-      backgroundColor: "#a29bfe",
-      borderColor: "#a29bfe",
-      borderWidth: 1,
-    },
-  ],
-};
 
-const BarChart = () => {
+const BarChart = (props) => {
+  
+  const data = {
+    labels: props.revenue.years,//["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    datasets: [
+      {
+        label: "Users",
+        data: props.revenue.users,//[33, 53, 85, 41, 44, 65],
+        backgroundColor: "#a29bfe",
+        borderColor: "#a29bfe",
+        borderWidth: 1,
+      },
+    ],
+  };
     return ( 
-    <div>
+      <div className="card border-0 bg-white rounded shadow-sm m-2"style={{ height: "320px" }} >
+                <div className="card-body">
+    <div className="col">
     <Bar data={data} />
+    </div>
+    </div>
     </div>
     )
 }
