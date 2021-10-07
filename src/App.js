a42ed6824592a1b2a80dc73966abef025ff47e2b
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-10-07 10:58:54 +0530

    login functionality added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import { ToastContainer, toast } from "react-toastify";
-
+import { Provider } from "react-redux";
+import store from "./App/Redux/store";
 import Dashboard from './App/Pages/Dashboard';
 import Login from './App/Pages/Login';
 import PageNotFound from './App/Pages/PageNotFound';
 import NewsItem from './App/Pages/NewsItem';
+import { UserContext } from './App/Utils/UserContext';
 toast.configure();
 
 
@@ -14,15 +16,17 @@ toast.configure();
 function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Login} />
-          <Route path="/user/:slug" component={Dashboard} />
-          <Route path="/news/:slug" component={NewsItem} />
+      <Provider store={store}>
+        <Router>
+            <Switch>
+              <Route path="/" exact component={Login} />
+              <Route path="/user/:slug" component={Dashboard} />
+              <Route path="/news/:slug" component={NewsItem} />
 
-          <Route path="*" component={PageNotFound} />
-        </Switch>
-      </Router>
+              <Route path="*" component={PageNotFound} />
+            </Switch>
+        </Router>
+      </Provider>
     </>
   );
 }
---
+++ src/App/Redux/Actions/auth.js
@@ -0,0 +1,44 @@
+import {
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+} from "../Actions/types";
+import api from "../../Utils/api";
+
+
+//user login
+export const loginUser = (user) => async (dispatch) => {
+  const body = JSON.stringify(user);
+
+  try {
+    const res = await api.post("/login", body);
+    console.log("response at req", res);
+    if (res.status==200) {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+      return res.data;
+    } else {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: res.status,
+      });
+      return res.data;
+    }
+  } catch (error) {
+    console.log("error in catch", error);
+    dispatch({
+      type: LOGIN_FAIL,
+      payload: "Something went wrong!",
+    });
+  }
+};
+
+//logout user
+export const logoutUser = () => (dispatch) => {
+  localStorage.removeItem("token");
+  dispatch({
+    type: LOGOUT,
+  });
+};
